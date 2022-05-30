5315ee568d000dfb5d05598b187b90bc6442f6ba
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-30 11:06:42 +0200

    ADD : update delete

--- index.php
+++ index.php
@@ -12,27 +12,41 @@ if (file_exists('test.xml')) {
     
 
         // Requete d'insertion
-        $sql = "INSERT INTO article (titre, description, prix) VALUES (?, ?, ?) ";
+        // $sql = "INSERT INTO article (titre, description, prix) VALUES (?, ?, ?) ";
+        // $stmt = $db->prepare($sql);
+        // $stmt->bindValue(1,"SamesungS22");
+        // $stmt->bindValue(2,"La concurrene");
+        // $stmt->bindValue(3,"1200");
+        // $stmt->execute();
+
+        // Requete de supprimer
+        // $sql = "DELETE FROM article WHERE id = ? ";
+        // $stmt = $db->prepare($sql);
+        // $stmt->bindValue(1,1);
+        // $stmt->execute();
+
+        // Requete de mise à jour
+        $sql = "UPDATE  article SET prix = ? WHERE id = ?";
         $stmt = $db->prepare($sql);
-        $stmt->bindValue(1,"SamesungS22");
-        $stmt->bindValue(2,"La concurrene");
-        $stmt->bindValue(3,"1200");
+        $stmt->bindValue(1,50);
+        $stmt->bindValue(2,3);
         $stmt->execute();
 
-
+        
         //Requete de selectAll
-        // $sql = "SELECT * FROM article";
-        // $stmt = $db->query($sql);
-        // $arr = $stmt->fetchAll(PDO::FETCH_ASSOC); // tableau
-        // var_dump($arr);
+        $sql = "SELECT * FROM article";
+        $stmt = $db->query($sql);
+        $arr = $stmt->fetchAll(PDO::FETCH_ASSOC); // tableau
+        var_dump($arr);
 
         // Requete de selecBy
-        $sql = "SELECT * FROM article WHERE id = ? ";
-        $stmt = $db->prepare($sql);
-        $stmt->bindValue(1,4);
-        $stmt->execute();
-        $one = $stmt->fetch(PDO::FETCH_ASSOC);
-        var_dump($one);
+        // $sql = "SELECT * FROM article WHERE id = ? ";
+        // $stmt = $db->prepare($sql);
+        // $stmt->bindValue(1,4);
+        // $stmt->execute();
+        // $one = $stmt->fetch(PDO::FETCH_ASSOC);
+        // var_dump($one);
+
 
     }catch(PDOException $e ){
         echo "Not Connected";
